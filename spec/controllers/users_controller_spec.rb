008307e1db5641c9ca34f58a49e3ccb60c1f59bc
Greg RV <gregvarias@gmail.com>
2014-10-02 21:22:02 -0700

    refactored UsersController to use subject and before hooks

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,41 +2,45 @@ require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
 	describe "#new" do
-		it "renders the 'new' template" do
+		before(:each) do
 			get "new"
+		end
+
+		it "renders the 'new' template" do
 			expect(response).to render_template("new")
 		end
 
 		it "assigns a new instance of User" do
-			get "new"
 			expect(assigns(:user)).to be_instance_of(User)
 		end
 	end
 
 	describe "#create" do
 		context "with valid info" do
+			subject {post "create", user: {name: 'Alan', email: 'alan@test.com', password: 'alan', password_confirmation: 'alan'}}
+
 			it "it creates a new user" do
-			expect {
-				post "create", user: {name: 'Alan', email: 'alan@test.com', password: 'alan', password_confirmation: 'alan'}
-				}.to change(User, :count).by(1)
+				#use {} here instead of () because
+				#subject represents a block
+				expect{subject}.to change(User, :count).by(1)
 			end
 
 			it "redirects to user#show" do
+				#kept following line because the spec
+				#needs to refer to :user
 				post "create", user: {name: 'Alan', email: 'alan@test.com', password: 'alan', password_confirmation: 'alan'}
 				expect(response).to redirect_to(assigns(:user))
 			end
 		end
 
 		context "with invalid info" do
+			subject {post "create", user: {name: '', email: 'alan@test.com', password: 'alan', password_confirmation: 'alan'}}
 			it "does not create a new user" do
-				expect {
-					post "create", user: {name: '', email: 'alan@test.com', password: 'alan', password_confirmation: 'alan'}
-				}.to_not change(User, :count)
+				expect{subject}.to_not change(User, :count)
 			end
 
 			it "renders user#new" do
-				post "create", user: {name: '', email: 'alan@test.com', password: 'alan', password_confirmation: 'alan'}
-				expect(response).to render_template("new")
+				expect(subject).to render_template("new")
 			end
 		end
 	end
---
+++ spec/factories/factory.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name 'Alan'
+		email 'alan@test.com'
+		password 'alan'
+		password_confirmation 'alan'
+	end
+end
